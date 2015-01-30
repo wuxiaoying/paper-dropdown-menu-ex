fb222afc3346d786c10ec349f53f4e1e4219e512
helenxywu <xiaoyingw12@gmail.com>
2015-01-29 20:43:52 -0800

    initial commit

---
+++ paper-dropdown-ex.js
@@ -0,0 +1,10 @@
+(function() {
+  Polymer({
+    positionTarget: function() {
+      this.style.left = "0";
+      this.style.top = "29px";
+      this.style.right = "0";
+    }
+  });
+
+}).call(this);
---
+++ paper-dropdown-menu-ex.coffee
@@ -0,0 +1,4 @@
+Polymer
+  attached: ->
+    @$.dropdown.appendChild this.children[0]
+    return
