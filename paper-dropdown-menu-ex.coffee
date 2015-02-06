6e0d9f1134e488fec6ea2612fb2d14af6a5b6a41
unknown <helen.wu@clapham.laserfiche.com>
2015-02-05 18:39:44 -0800

    add key bindings

--- paper-dropdown-menu-ex.coffee
+++ paper-dropdown-menu-ex.coffee
@@ -1,7 +1,11 @@
 Polymer
   disabled: false
 
+  observe:
+    '$.dropdown.opened': 'onClose'
+
   domReady: ->
+    @selector = this.children[0].querySelector '.selector'
     @$.dropdown.appendChild this.children[0]
     @input = @$.decorator.input
     return
@@ -12,4 +16,22 @@ Polymer
 
   update: (value) ->
     @$.decorator.update value
+    return
+
+  keyHandler: (e, detail) ->
+    return if not @selector
+
+    @$.dropdown.opened = true
+    switch detail.key
+      when 'down'
+        @selector.selectNext()
+      when 'up'
+        @selector.selectPrevious()
+
+    e.stopPropagation()
+    return
+
+  onClose: (e, detail) ->
+    if not detail
+      @$.dropdown.focus()
     return
--- paper-dropdown-menu-ex.js
+++ paper-dropdown-menu-ex.js
@@ -1,7 +1,11 @@
 (function() {
   Polymer({
     disabled: false,
+    observe: {
+      '$.dropdown.opened': 'onClose'
+    },
     domReady: function() {
+      this.selector = this.children[0].querySelector('.selector');
       this.$.dropdown.appendChild(this.children[0]);
       this.input = this.$.decorator.input;
     },
@@ -10,6 +14,25 @@
     },
     update: function(value) {
       this.$.decorator.update(value);
+    },
+    keyHandler: function(e, detail) {
+      if (!this.selector) {
+        return;
+      }
+      this.$.dropdown.opened = true;
+      switch (detail.key) {
+        case 'down':
+          this.selector.selectNext();
+          break;
+        case 'up':
+          this.selector.selectPrevious();
+      }
+      e.stopPropagation();
+    },
+    onClose: function(e, detail) {
+      if (!detail) {
+        this.$.dropdown.focus();
+      }
     }
   });
 
